e565cc0577cc73d931567f46e05ebe886523e776
Martin Mosegaard Amdisen <martinmosegaard@gmail.com>
2016-06-18 10:21:43 +0200

    Make the AlphabetizeView more groovy

--- src/main/groovy/dk/martinmosegaard/registrysorter/view/AlphabetizeView.groovy
+++ src/main/groovy/dk/martinmosegaard/registrysorter/view/AlphabetizeView.groovy
@@ -1,53 +1,53 @@
-package dk.martinmosegaard.registrysorter.view;
+package dk.martinmosegaard.registrysorter.view
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout
+import java.awt.Color
+import java.awt.event.ActionListener
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.WindowConstants;
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JPanel
+import javax.swing.JScrollPane
+import javax.swing.JTextArea
+import javax.swing.WindowConstants
 
-public class AlphabetizeView extends JFrame {
+class AlphabetizeView extends JFrame {
 
-  private static final long serialVersionUID = 1;
+  private static final long serialVersionUID = 1
 
-  private JButton alphabetizeButton;
-  private JTextArea textArea;
+  private JButton alphabetizeButton
+  private JTextArea textArea
 
-  public AlphabetizeView(String title) {
-    setTitle(title);
-    setSize(800, 600);
-    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    setBackground(Color.gray);
-    getContentPane().setLayout(new BorderLayout());
+  AlphabetizeView(String title) {
+    setTitle(title)
+    setSize(800, 600)
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+    setBackground(Color.gray)
+    getContentPane().setLayout(new BorderLayout())
 
-    alphabetizeButton = new JButton("Alfabetiser");
-    getContentPane().add(alphabetizeButton, BorderLayout.NORTH);
+    alphabetizeButton = new JButton('Alfabetiser')
+    getContentPane().add(alphabetizeButton, BorderLayout.NORTH)
 
-    JPanel topPanel = new JPanel();
-    topPanel.setLayout(new BorderLayout());
-    getContentPane().add(topPanel, BorderLayout.CENTER);
+    JPanel topPanel = new JPanel()
+    topPanel.setLayout(new BorderLayout())
+    getContentPane().add(topPanel, BorderLayout.CENTER)
 
-    textArea = new JTextArea();
-    JScrollPane scroller = new JScrollPane();
-    scroller.getViewport().add(textArea);
-    topPanel.add(scroller, BorderLayout.CENTER);
+    textArea = new JTextArea()
+    JScrollPane scroller = new JScrollPane()
+    scroller.getViewport().add(textArea)
+    topPanel.add(scroller, BorderLayout.CENTER)
   }
 
-  public void setListener(ActionListener listener) {
-    alphabetizeButton.addActionListener(listener);
+  void setListener(ActionListener listener) {
+    alphabetizeButton.addActionListener(listener)
   }
 
-  public String getText() {
-    return textArea.getText();
+  String getText() {
+    return textArea.getText()
   }
 
-  public void setText(String text) {
-    textArea.setText(text);
+  void setText(String text) {
+    textArea.setText(text)
   }
 
 }
