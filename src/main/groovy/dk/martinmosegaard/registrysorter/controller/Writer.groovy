f4a1a9463625ab85683dae9c0041d7becf3b6c83
Martin Mosegaard Amdisen <martinmosegaard@gmail.com>
2016-06-18 10:38:22 +0200

    Groovify Writer class

--- src/main/groovy/dk/martinmosegaard/registrysorter/controller/Writer.groovy
+++ src/main/groovy/dk/martinmosegaard/registrysorter/controller/Writer.groovy
@@ -1,22 +1,22 @@
-package dk.martinmosegaard.registrysorter.controller;
+package dk.martinmosegaard.registrysorter.controller
 
-import dk.martinmosegaard.registrysorter.model.RegistryEntry;
+import dk.martinmosegaard.registrysorter.model.RegistryEntry
 
 class Writer {
 
   String write(RegistryEntry rootEntry) {
-    StringBuffer buffer = new StringBuffer();
-    write(buffer, rootEntry);
-    return buffer.toString();
+    def buffer = new StringBuffer()
+    write(buffer, rootEntry)
+    buffer.toString()
   }
 
   void write(StringBuffer buffer, RegistryEntry entry) {
     if (entry.getLine() != "") {
-      buffer.append(entry.getLine());
-      buffer.append(System.lineSeparator());
+      buffer.append(entry.getLine())
+      buffer.append(System.lineSeparator())
     }
-    for (RegistryEntry child : entry.getChildren()) {
-      write(buffer, child);
+    entry.children.each { RegistryEntry child ->
+      write(buffer, child)
     }
   }
 
