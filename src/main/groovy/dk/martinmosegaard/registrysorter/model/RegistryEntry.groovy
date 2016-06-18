2223319b78403faa644329356cabdd9994dd3ed2
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-06-18 21:37:07 +0200

    Make RegistryEntry a bit more groovy

--- src/main/groovy/dk/martinmosegaard/registrysorter/model/RegistryEntry.groovy
+++ src/main/groovy/dk/martinmosegaard/registrysorter/model/RegistryEntry.groovy
@@ -1,35 +1,32 @@
-package dk.martinmosegaard.registrysorter.model;
+package dk.martinmosegaard.registrysorter.model
 
-import java.util.ArrayList;
-import java.util.List;
+class RegistryEntry {
 
-public class RegistryEntry {
+  private final RegistryEntry parent
+  private final int indent
+  private final String line
+  private final List<RegistryEntry> children = new ArrayList<>()
 
-  private final RegistryEntry parent;
-  private final int indent;
-  private final String line;
-  private final List<RegistryEntry> children = new ArrayList<>();
-
-  public RegistryEntry(RegistryEntry parent, int indent, String line) {
-    this.parent = parent;
-    this.indent = indent;
-    this.line = line;
+  RegistryEntry(RegistryEntry parent, int indent, String line) {
+    this.parent = parent
+    this.indent = indent
+    this.line = line
   }
 
-  public RegistryEntry getParent() {
-    return parent;
+  RegistryEntry getParent() {
+    return parent
   }
 
-  public int getIndent() {
-    return indent;
+  int getIndent() {
+    return indent
   }
 
-  public String getLine() {
-    return line;
+  String getLine() {
+    return line
   }
 
-  public List<RegistryEntry> getChildren() {
-    return children;
+  List<RegistryEntry> getChildren() {
+    return children
   }
 
 }
