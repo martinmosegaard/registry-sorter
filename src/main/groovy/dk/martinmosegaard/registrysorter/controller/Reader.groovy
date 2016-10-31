6578522f9ba5a2287ad1053f5096a7015bebdf6f
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-10-31 19:04:06 +0100

    Debugging RTF

--- src/main/groovy/dk/martinmosegaard/registrysorter/controller/Reader.groovy
+++ src/main/groovy/dk/martinmosegaard/registrysorter/controller/Reader.groovy
@@ -66,6 +66,9 @@ class Reader {
     List<RegistryEntry> currentEntries = currentParent.children
     RegistryEntry lastEntry = null
 
+    println '>>> reader'
+    document.dump(System.out)
+    println '<<< reader'
     Element rootElement = document.defaultRootElement
 
     for (int i = 0; i < rootElement.elementCount; ++i) {
